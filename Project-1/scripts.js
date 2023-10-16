ee7d2f2e2a16ea1fe23e21cfd54f5bd4379a8393
asma <benramoulasma@gmail.com>
2023-10-16 21:18:46 +0100

    quotes edit

--- Project-1/scripts.js
+++ Project-1/scripts.js
@@ -1,14 +1,14 @@
 const quotes = [
-    "Inhale peace exhale stress.",
-    "Find joy in the ordinary.",
-    "Every day may not be good, but there is something good in every day.",
-    "Calmness is the cradle of power.",
-    "This pain that you feel can't compare to the joy that's coming.",
-    "Kindness is a language that the deaf can hear and the blind can see.",
-    "Forgivness is the final form of love, also a virtue od the brave.",
-    "Sometimes the most productive thing you can do is to relax.",
-    "Only those you love can break your heart.",
-    "Peace begins with a smile."
+    '"Inhale peace exhale stress."',
+    '"Find joy in the ordinary."',
+    '"Every day may not be good, but there is something good in every day."',
+    '"Calmness is the cradle of power."',
+    '"This pain that you feel can\'t compare to the joy that\'s coming."',
+    '"Kindness is a language that the deaf can hear and the blind can see."',
+    '"Forgivness is the final form of love, also a virtue od the brave."',
+    '"Sometimes the most productive thing you can do is to relax."',
+    '"Only those you love can break your heart."',
+    '"Peace begins with a smile."'
 ]
 
 const Text = document.querySelector("#Text")
