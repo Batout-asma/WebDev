4975009f9ceeaa09a64b4d0df2145414833c4aba
asma <benramoulasma@gmail.com>
2023-10-19 20:21:33 +0100

    edit

--- Project-2/scripts.js
+++ Project-2/scripts.js
@@ -9,12 +9,28 @@ number.textContent = cpt;
 incrementBtn.addEventListener("click", () => {
     cpt++;
     number.textContent = cpt;
+    changeClass()
 })
 decrementBtn.addEventListener("click", () => {
     cpt--;
     number.textContent = cpt;
+    changeClass()
 })
 clearBtn.addEventListener("click", () => {
     cpt = 0;
     number.textContent = cpt;
+    changeClass()
 })
+function changeClass() {
+    if (cpt > 0) {
+        number.className = ""
+        number.classList.add("positive")
+    }
+    else if (cpt == 0) {
+        number.className = ""
+    }
+    else {
+        number.className = ""
+        number.classList.add("negative")
+    }
+}
